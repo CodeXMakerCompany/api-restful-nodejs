26bca9f8d9c9f1d8d89c0bd1270707cefc7c4e5e
codexmaker <codexmaker12@gmail.com>
2019-12-27 15:14:33 -0600

    updatingApi

--- controllers/user.js
+++ controllers/user.js
@@ -9,6 +9,10 @@ var User = require('../models/user');
 //Cargar libreria de encriptado
 var bcrypt = require('bcryptjs');
 
+//Servicios
+//token jwt
+var jwt = require('../services/jwt');
+
 var controller = {
 
   probando: function(req, res){
@@ -98,6 +102,73 @@ var controller = {
         message: "Validacion de los datos del usuario incorrecta. Intentelo de nuevo."
       });
     }
+
+  },
+
+  login: function(req, res){
+    //Recoger los parametros de la petición
+    var params = req.body;
+
+    //Validar los datos
+    var validate_email    = !validator.isEmpty(params.email) && validator.isEmail(params.email);
+    var validate_password = !validator.isEmpty(params.password);
+
+    if (!validate_email || !validate_password) {
+      return res.status(200).send({
+        message: "Datos incorrectos, envielos bien."
+      });
+    }
+      //Buscar usuarios que coincidan con el email
+      User.findOne({email: params.email.toLowerCase()}, (err, user) =>{
+
+        if (err) {
+          return res.status(500).send({
+            message: "Error al intentar identificrse"
+          });
+        }
+
+        if (!user) {
+          return res.status(404).send({
+            message: "El usuario no existe"
+          });
+        }
+
+        //Si lo encuentra
+        //Comprobar que la contraseña (conicidencia de email y password / bcryptjs)
+        bcrypt.compare(params.password, user.password, (err, check) => {
+
+          //Si es correcto
+          if (check) {
+
+            //Generar token de jwt y devlverlo(mas tarde)
+            if (params.gettoken) {
+              //Devolver datos
+              return res.status(200).send({
+                token: jwt.createToken(user)
+              });
+            }else{
+              //Limpiar objeto eliminar propiedades
+              user.password = undefined;
+              //Devolver datos
+              return res.status(200).send({
+                status: "success",
+                user
+              });
+            }
+
+          }else{
+
+            return res.status(200).send({
+              message: "Las credenciales no son correctas"
+            });
+          }
+
+        });
+
+
+      });
+
+
 
   }
 
